8163c2506b10142b4a2fd2842fd658729b5afcc6
tsuminetic <diensanc@gmail.com>
2024-05-02 12:47:16 +0545

    format

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,13 @@
 from api.auth_api import api as auth_api
 from api.note_api import api as note_api
 from api.example_api import api as example_api
+from api.list_api import api as list_api
+
 
 def initialize_routes(app):
     app.register_blueprint(note_api, url_prefix='/')
     app.register_blueprint(auth_api, url_prefix='/')
     app.register_blueprint(example_api, url_prefix='/')
+    app.register_blueprint(list_api, url_prefix='/')
 
 
