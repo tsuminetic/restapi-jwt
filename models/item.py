1a8052d33bfb7c6b42aa8acb9372cf126a30d375
tsuminetic <diensanc@gmail.com>
2024-05-02 12:47:36 +0545

    format

--- models/item.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class ListItem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(1000))
-    list_id=db.Column(db.Integer,db.ForeignKey('list.id'))
-    
